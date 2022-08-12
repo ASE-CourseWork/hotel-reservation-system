24d78002ec945ee0cccfb344bf5bc87c1ef6ac9a
Risiidhan <rkishon4@gmail.com>
2022-08-12 17:52:46 +0530

    fix error

--- Client/js/customer.js
+++ Client/js/customer.js
@@ -44,22 +44,22 @@ function add(){
         let roomsElements = document.getElementsByClassName('roomes')[0];
         let roomDivContent = 
         `  
-            <div class="row">
+        <div class="row">
             <div class="col">
-            <h3>Best Available Rate</h3>
-            <h4>${rooms[i].type}</h4>
-            <p>Per Night | ${rooms[i].noOfPeople} People</p>
+                <h3>Best Available Rate</h3>
+                <h4>${rooms[i].type}</h4>
+                <p>Per Night | ${rooms[i].noOfPeople} People</p>
             </div>
             <div class="col">
-            <img class="imm" src=${rooms[i].imgUrl}>
+                <img class="imm" src=${rooms[i].imgUrl}>
             </div>
         </div>
         <div class="row">
             <div class="col">
-            <h4 class="pri">${rooms[i].price} </h4>
+                <h4 class="pri">${rooms[i].price} </h4>
             </div>
             <div class="col">
-            <input type="button" value="Select" class="btn btn-success" id="select">
+                <input type="button" value="Select" class="btn btn-success" id="select">
             </div>
         </div> 
         `;
